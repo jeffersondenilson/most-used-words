06dba6533f5833cb713b455b9793559fd30f5bfb
jeffersondenilson <jefersond121212@gmail.com>
2020-11-11 13:43:26 -0300

    cores

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,8 @@
 <template>
 	<v-container fluid>
 			<Pill 
-				v-for="word in groupedWords" :key="word.name"
-				:name="word.name" :amount="word.amount" />
+				v-for="(word, index) in groupedWords" :key="word.name"
+				:name="word.name" :amount="word.amount" :color="pillColor(index)" />
 	</v-container>
 </template>
 
@@ -24,31 +24,81 @@
 				{ name: 'yu', amount: 800 },
 				{ name: 'h', amount: 123 },
 				{ name: 'i', amount: 12 },
-				{ name: 'u', amount: 9 },
+				{ name: 'uboqw', amount: 9 },
 				{ name: 'e', amount: 8 },
 			],
 			colors: [
+				/*
 				'red accent-4', 
-				'pink', 
+				'pink accent-3', 
 				'purple', 
 				'blue', 
 				'green', 
-				'yellow', 
-				'grey'
+				'yellow darken-1', 
+				'grey darken-1'
+				*/
+				/*
+				'red accent-4',
+				'purple',
+				'blue',
+				'cyan',
+				'green',
+				'yellow darken-1', 
+				'grey darken-1'
+				*/
+				
+				'red accent-4',
+				'orange',
+				'yellow accent-4',
+				'green',
+				'cyan',
+				'blue darken-1',
+				'grey darken-1'
+
 			],
 		}),
 
 		methods: {
-			calculateTotalWords(){
-				this.wordsTotal = groupedWords.reduce((total, word)=>{
-					return total + word.amount;
-				});
-			},
-
-			amountToColor(amount){
-				const factor = groupedWords.length / 7;
-				// return color;
+			// seleciona cor de acordo com o item da repetição
+			pillColor(position){
+				// porcentagem dos items percorridos
+				let positionPercent = position / this.groupedWords.length;
+				// cor correspendente em this.colors pela porcentagem
+				let colorsPosition = Math.floor(positionPercent * this.colors.length);
+				return this.colors[colorsPosition];
 			}
 		},
 	}
 </script>
+
+<!-- 
+colors: [
+	/*
+	'red accent-4', 
+	'pink accent-3', 
+	'purple', 
+	'blue', 
+	'green', 
+	'yellow darken-1', 
+	'grey darken-1'
+	*/
+	/*
+	'red accent-4',
+	'purple',
+	'blue',
+	'cyan',
+	'green',
+	'yellow darken-1', 
+	'grey darken-1'
+	*/
+	/*
+	'red accent-4',
+	'orange',
+	'yellow accent-4',
+	'green',
+	'cyan',
+	'blue darken-1',
+	'grey darken-1'
+	*/
+],
+ -->
--- src/components/Pill.vue
+++ src/components/Pill.vue
@@ -18,7 +18,7 @@
 					{{ amount }}
 				</v-chip>
       </template>
-      <span v-comma-separated-number>{{ amount }}</span>
+      <span>{{ amount }}</span>
     </v-tooltip>
 	</v-chip>
 </template>
@@ -40,50 +40,26 @@
 		}
 	});
 
-	Vue.directive('comma-separated-number', {
-		inserted(el, binding){
-			// 1
-			const amount = el.innerText;
-			let newText = '';
-
-			for(let i = amount.length; i > 0; i-=3){
-				let substring = ',' + amount.substring(i, i-3);
-				newText = substring + newText;
-			}
-
-			if(newText[0] === ','){
-				newText[0] = '';
-			}
-
-			el.innerText = newText;
-			// 2
-			// const array = el.innerText.split(''); 
-			// let newText = '', j = 0;
-			// for(let i = array.length; i > 0; i--){
-			// 	newText = array[i] + newText;
-			// 	j++;
-			// 	if(j===3){
-			// 		newText+=',';
-			// 		j=0;
-			// 	}
-			// }
-			// el.innerText = newText;
-		}
-	});
-
 	export default {
 		props: ['name', 'amount', 'color'],
 		name: 'Pill',
-		data: () => ({
-			colors: [
-				'red accent-4', 
-				'pink', 
-				'purple', 
-				'blue', 
-				'green', 
-				'yellow', 
-				'grey'
-			]
-		})
 	}
 </script>
+
+<!-- 
+red
+purple
+blue
+cyan
+green
+yellow
+gray
+
+red
+orange
+yellow
+green
+cyan
+blue
+black
+-->
